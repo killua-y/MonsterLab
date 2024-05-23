88347b6f9e4dd8b90c89a060822f5b1b0c6aba5b
Xinghan Yan <76619533+killua-y@users.noreply.github.com>
2024-05-23 21:43:44 +0800

    5/23

--- Assets/Script/Manager/Tower/ActsManager.cs
+++ Assets/Script/Manager/Tower/ActsManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class ActsManager : MonoBehaviour
 {
+    private CardDataModel cardData;
+    public Transform Deck;
+    public Transform drawPileScroll;
+    public Transform drawPileScrollContent;
+    public Transform discardPileScoll;
+    public Transform discardPileScollContent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cardData = FindObjectOfType<CardDataModel>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OpenDeck()
+    {
+        if (Deck.gameObject.activeSelf)
+        {
+            Deck.gameObject.SetActive(false);
+        }
+        else
+        {
+            Deck.gameObject.SetActive(true);
+        }
+    }
+
+    private void ShowDarwPile()
+    {
+
+    }
+
+    private void ShowDiscardPile()
+    {
+
     }
 }
